bdeafcf2596d9b61b62f48a1fb20bbaba75ee769
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-13 14:19:46 +0300

    Implement create and show in console

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ Entry point of the command interpreter.
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
     Command interpreter class.
     """
     prompt = "(hbnb) "
+    __models = {"BaseModel", }
 
     def do_quit(self, arg):
         """
@@ -30,6 +33,47 @@ class HBNBCommand(cmd.Cmd):
         Empty line + ENTER shouldn’t execute anything.
         """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id.
+
+        Args:
+            arg (str): Argument to enter with command: <class_name>
+            Example: create BaseModel
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.__models:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class
+        name and id.
+
+        Args:
+            arg (str): Argument to enter with command: <class_name> <id>
+            Example: show BaseModel 1234-1234-1234
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__models:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
 
 
 if __name__ == "__main__":
