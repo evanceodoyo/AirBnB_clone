6d2e1f5ce099aacb1c3bfbd31a005a35d90f687b
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-13 13:40:14 +0300

    Add basic console commands

--- console.py
+++ console.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+
+"""
+Entry point of the command interpreter.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter class.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program.
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Empty line + ENTER shouldn’t execute anything.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
