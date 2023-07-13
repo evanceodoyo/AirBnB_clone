54e8a4a1c3a0bee22cc61cc8f92dd511809b0c11
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-13 19:12:16 +0300

    Modify console/command intepreter to accept class and methods as args.

--- console.py
+++ console.py
@@ -153,6 +153,56 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
+    def do_count(self, arg):
+        """
+        Retrieve the number of instances of a class.
+
+        Args:
+            arg (str): Argument to enter with command: <class_name>
+            Example: count BaseModel
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__models:
+            print("** class doesn't exist **")
+        else:
+            print(len([key for key in storage.all()
+                       if key.startswith(args[0])]))
+
+    def default(self, arg):
+        """
+        Retrieve all instances of a class by using: <class name>.all().
+        Retrieve the number of instances of a class by using:
+        <class name>.count().
+        Retrieve an instance based on its ID: <class name>.show(<id>).
+        Destroy an instance based on his ID: <class name>.destroy(<id>).
+        Update an instance based on his ID: <class name>.update(<id>,
+        <attribute name>, <attribute value>).
+
+        Args:
+            arg (str): Argument to enter with command: <class_name>.<method>()
+            Example: User.all()
+        """
+        args = arg.split(".")
+        if len(args) > 1:
+            if args[1] == "all()":
+                self.do_all(args[0])
+            elif args[1] == "count()":
+                self.do_count(args[0])
+            elif args[1].startswith("show("):
+                id = args[1].split("(")[1].split(")")[0]
+                self.do_show(args[0] + " " + id)
+            elif args[1].startswith("destroy("):
+                id = args[1].split("(")[1].split(")")[0]
+                self.do_destroy(args[0] + " " + id)
+            elif args[1].startswith("update("):
+                id = args[1].split("(")[1].split(",")[0]
+                attribute = args[1].split("(")[1].split(",")[1]
+                value = args[1].split("(")[1].split(",")[2].split(")")[0]
+                self.do_update(args[0] + " " + id + " " + attribute + " " +
+                               value)
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
